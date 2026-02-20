e54e8263332b1d66fc1c488d3f2aafcd956aa1d5
Damian Fahy <30182121+DFahy@users.noreply.github.com>
2026-02-20 17:58:47 -0600

    Updated xirr.ts

--- src/utils/xirr.ts
+++ src/utils/xirr.ts
@@ -37,7 +37,7 @@ function dateDiffInDays(date1: Date, date2: Date): number {
   const msPerDay = 1000 * 60 * 60 * 24;
   const utc1 = Date.UTC(date1.getFullYear(), date1.getMonth(), date1.getDate());
   const utc2 = Date.UTC(date2.getFullYear(), date2.getMonth(), date2.getDate());
-  return Math.abs((utc2 - utc1) / msPerDay);
+  return Math.abs((utc2 - utc1) / msPerDay) + 1;
 }
 
 function calculateNPV(rate: number, cashFlows: CashFlow[], startDate: Date): number {
