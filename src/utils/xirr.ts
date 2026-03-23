198668a9d77d33a6551503d70d599a2228fe3253
Damian Fahy <30182121+DFahy@users.noreply.github.com>
2026-03-23 10:58:24 -0500

    Update XIRR day count to inclusive

--- src/utils/xirr.ts
+++ src/utils/xirr.ts
@@ -36,7 +36,8 @@ function dateDiffInDays(date1: Date, date2: Date): number {
   const msPerDay = 1000 * 60 * 60 * 24;
   const utc1 = Date.UTC(date1.getFullYear(), date1.getMonth(), date1.getDate());
   const utc2 = Date.UTC(date2.getFullYear(), date2.getMonth(), date2.getDate());
-  return Math.abs((utc2 - utc1) / msPerDay);
+  // Add 1 to match Excel's XIRR inclusive day count (both start and end dates count)
+  return Math.abs((utc2 - utc1) / msPerDay) + 1;
 }
 
 function calculateNPV(rate: number, cashFlows: CashFlow[], startDate: Date): number {
